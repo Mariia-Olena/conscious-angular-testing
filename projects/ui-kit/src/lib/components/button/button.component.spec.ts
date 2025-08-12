d7921acce21a56a61b88928c1aa5dc25f11908e6
Mariia-Olena Stus <mariia-olena.stus@hys-enterprise.com>
2025-08-12 17:02:32 +0300

    add disabled state

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -21,31 +21,45 @@ describe('ButtonComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should have "solid" appearance by default', () => {
-    // expect(debugElement.classes['solid-button']).toBe(true);
-    // expect(element.classList.contains('solid-button')).toBe(true);
-    expect(element.classList).toContain('solid-button');
+  describe('Appearance state', () => {
+    it('should have "solid" appearance by default', () => {
+      // expect(debugElement.classes['solid-button']).toBe(true);
+      // expect(element.classList.contains('solid-button')).toBe(true);
+      expect(element.classList).toContain('solid-button');
+    });
+
+    it('should apply proper CSS classes when appearance is changed', () => {
+      component.appearance = 'stroked';
+      fixture.detectChanges();
+      expect(element.classList).toContain('stroked-button');
+
+      component.appearance = 'solid';
+      fixture.detectChanges();
+      expect(element.classList).toContain('solid-button');
+    });
   });
 
-  it('should apply proper CSS classes when appearance is changed', () => {
-    component.appearance = 'stroked';
-    fixture.detectChanges();
-    expect(element.classList).toContain('stroked-button');
+  describe('Loading state', () => {
+    it('should show loader icon in "loading" state', () => {
+      fixture.componentRef.setInput('loading', true);
+      fixture.detectChanges();
+      let loader = debugElement.query(By.css('[data-testingId="loader"]'));
+      expect(loader).not.toBeNull();
 
-    component.appearance = 'solid';
-    fixture.detectChanges();
-    expect(element.classList).toContain('solid-button');
+      fixture.componentRef.setInput('loading', false);
+      fixture.detectChanges();
+      loader = debugElement.query(By.css('[data-testingId="loader"]'));
+      expect(loader).toBeNull();
+    });
   });
 
-  it('should show loader icon in "loading" state', () => {
-    fixture.componentRef.setInput('loading', true);
-    fixture.detectChanges();
-    let loader = debugElement.query(By.css('[data-testingId="loader"]'));
-    expect(loader).not.toBeNull();
-
-    fixture.componentRef.setInput('loading', false);
-    fixture.detectChanges();
-    loader = debugElement.query(By.css('[data-testingId="loader"]'));
-    expect(loader).toBeNull();
+  describe('Disabled state', () => {
+    it('should apply necessary attributes to component host', () => {
+      fixture.componentRef.setInput('disabled', true);
+      fixture.detectChanges();
+      expect(element.classList).toContain('disabled');
+      expect(element.getAttribute('disabled')).not.toBeNull();
+      expect(element.getAttribute('tabindex')).toBe('-1');
+    });
   });
 });
